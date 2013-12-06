4d7081c5626a8855b6212af416bf8a3a1b4321ab
syamgk <syamgk@gmail.com>
2013-12-06 21:56:57 +0530

    score ready

--- puzzle.py
+++ puzzle.py
@@ -1,11 +1,17 @@
 import random
-import pygame
 from time import sleep
 from subprocess import check_output
-from pygame.locals import *
+import thread
+try:
+	import pygame
+	from pygame.locals import *
+except ImportError:
+	print "Needs pygame library:\nYou can install it by typing:\nsudo apt-get instal python-pygame"
+	exit(0)
 #----------------------------------------------------------------------------------------------------------------------
 class Game(object):
 	def __init__(self):
+		pygame.init()
 		pygame.mixer.init()
 		color = Green = 144, 238, 144
 		self.get_resolution()
@@ -14,17 +20,19 @@ class Game(object):
 		pygame.display.set_caption("Match Images")	
 		pygame.event.set_allowed([QUIT,MOUSEBUTTONDOWN,MOUSEBUTTONUP])
 		self.shuffle()
+		self.score = 0
+		self.scoreboard(0)
 		self.corresponding_pos = []
 		self.check = []
 		self.matched = set()
 		self.draw_cards()
 	def get_resolution(self):
 # get screen sizes w.r.t current disply resolution
-		f = random.randint
+		f = random.choice
 		disply = check_output(["xrandr"]).split(", ")[1].split(" ")
 		self.screen_size = screen_w, screen_h = int(disply[1]), int(disply[-1]) -60
 		self.table_size = table_w, table_h = screen_w -80 ,screen_h -80
-		self.card_size = self.WIDTH,self.HEIGHT = table_w/4 ,table_h/f(2,8)
+		self.card_size = self.WIDTH,self.HEIGHT = table_w/f([4,6]) ,table_h/f([4,6])
 		self.card_pic_size = self.WIDTH -20 ,self.HEIGHT -20
 		self.num = (table_w *table_h)/(self.WIDTH*self.HEIGHT)
 		self.qm_size = self.i,self.j = self.WIDTH -40/2,self.HEIGHT -40/2
@@ -70,14 +78,15 @@ class Game(object):
 			self.screen.blit(self.blank_card,self.corresponding_pos[-2])
 			self.screen.blit(self.blank_card,self.corresponding_pos[-1])
 			self.sound("bump.wav")
+			if not self.score ==0 :self.scoreboard(-5)
 			pygame.display.flip()
 		elif not self.corresponding_pos[-1] == self.corresponding_pos[-2]:
 			self.sound("coin.wav")
+			self.scoreboard(10)
 			self.matched.add(self.corresponding_pos[-1])
 			self.matched.add(self.corresponding_pos[-2])
 			if len(self.matched) == self.num: self.sound("stage_clear.wav")
 		self.check = []
-		print self.corresponding_pos
 	def track_card(self ,event):
 #track cards w.r.t position
 		size, p, n = self.screen_size, event.pos, 0
@@ -88,6 +97,17 @@ class Game(object):
 					self.show_card(n,(x,y))
 					if len(self.check) == 2:self.check_prev()
 				n+=1
+	def scoreboard(self,point):
+		self.score += point
+		bg = pygame.Surface((150,20))
+		bg.fill((144, 238, 144))
+		font = pygame.font.Font(None, 30)
+		text = font.render("Score : "+str(self.score) , 1, (10, 10, 10))
+		textpos = text.get_rect()
+		textpos.centerx = bg.get_rect().centerx
+		bg.blit(text, (0, 0))
+		self.screen.blit(bg, (self.screen_size[0]-200, 10))
+		pygame.display.flip()
 	def handle_events(self):
 #handle events w.r.t clicks
 		for event in pygame.event.get():
@@ -96,6 +116,7 @@ class Game(object):
 				check_output(["python","puzzle.py"])
 			elif event.type==pygame.MOUSEBUTTONDOWN:
 				self.track_card(event)
+				print event
 G = Game()
 while True:
 	G.handle_events()
